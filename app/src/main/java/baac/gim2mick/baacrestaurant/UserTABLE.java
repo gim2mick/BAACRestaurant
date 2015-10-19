78fe8136fb554497b5286522de59c4ca0dd5ae98
gim2mick <chananya.c@gmail.com>
2015-10-19 14:27:13 +0700

    Create SQLite Success

--- app/src/main/java/baac/gim2mick/baacrestaurant/UserTABLE.java
+++ app/src/main/java/baac/gim2mick/baacrestaurant/UserTABLE.java
@@ -13,12 +13,16 @@ public class UserTABLE {
     private SQLiteDatabase writeSqLiteDatabase, readSqLiteDatabase;
 
     public static final String USER_TABLE ="userTABLE";
-    
+    public static final String COLUMN_ID_USER ="_id";
+    public static final String COLUMN_USER_USER ="User";
+    public static final String COLUMN_PASSWORD_USER ="Password";
+    public static final String COLUMN_NAME_USER ="Name";
 
     public UserTABLE(Context context) {
         objGim2mickOpenHelper = new gim2mickOpenHelper(context);
         writeSqLiteDatabase = objGim2mickOpenHelper.getWritableDatabase();
         readSqLiteDatabase = objGim2mickOpenHelper.getReadableDatabase();
     }//Constructor
+
 
 }//Main Class
