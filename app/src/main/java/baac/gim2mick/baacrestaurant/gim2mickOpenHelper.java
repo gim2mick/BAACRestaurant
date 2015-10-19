9fb0bb82d60c975d04e9a444df45d4859ef70c1c
gim2mick <chananya.c@gmail.com>
2015-10-19 13:07:53 +0700

    Start GitHub

---
+++ app/src/main/java/baac/gim2mick/baacrestaurant/gim2mickOpenHelper.java
@@ -0,0 +1,18 @@
+package baac.gim2mick.baacrestaurant;
+
+import android.content.Context;
+
+/**
+ * Created by gim2mick on 19/10/2015.
+ */
+public class gim2mickOpenHelper {
+
+    //Explicit
+    private static final String DATABASE_NAME = "BAAC.db";
+    private static final int DATABASE_VERSION = 1;
+
+
+    public gim2mickOpenHelper(Context context) {
+    }   // Constructor
+
+}   // Main Class
