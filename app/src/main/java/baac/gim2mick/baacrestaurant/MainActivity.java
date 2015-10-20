add04e27c937f62d142b99dca6fa8325aaca102b
gim2mick <chananya.c@gmail.com>
2015-10-20 09:54:51 +0700

    Delete All SQLite Success

--- app/src/main/java/baac/gim2mick/baacrestaurant/MainActivity.java
+++ app/src/main/java/baac/gim2mick/baacrestaurant/MainActivity.java
@@ -1,5 +1,6 @@
 package baac.gim2mick.baacrestaurant;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,10 +9,11 @@ public class MainActivity extends AppCompatActivity {
     //Explicit
     private UserTABLE objUserTABLE;
     private FoodTABLE objFoodTABLE;
-    private OrderTABLE objOrderTABLE;
+    //private OrderTABLE objOrderTABLE;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -19,25 +21,36 @@ public class MainActivity extends AppCompatActivity {
         createAndConnected();
 
         //test Add Value
-        testAddValue();
+        //testAddValue();
+
+        //Delete All SQLite
+        deleteAllSQLite();
 
     }   //Main Method
+
+    private void deleteAllSQLite() {
+
+        SQLiteDatabase objSqLiteDatabase = openOrCreateDatabase("BAAC.db", MODE_PRIVATE, null);
+        objSqLiteDatabase.delete("userTable", null, null);
+        objSqLiteDatabase.delete("foodTable", null, null);
+
+    }
+
+    private void testAddValue() {
+
+        objUserTABLE.addNewUser("testUser", "testPassword", "testName");
+        objFoodTABLE.addNewFood("testFood", "testSource", "testPrice");
+        //objOrderTABLE.addOrder("testOfficer", "testDesk", "testFood", "testItem");
+
+    } //test Add value
 
     private void createAndConnected() {
 
         objUserTABLE =new UserTABLE(this);
         objFoodTABLE = new FoodTABLE(this);
-        objOrderTABLE = new OrderTABLE(this);
+        //objOrderTABLE = new OrderTABLE(this);
 
     }// Create & Connect Database
-
-    private void testAddValue() {
-
-        //objUserTABLE.addNewUser("testUser","testPassword","testName");
-        //objFoodTABLE.addNewFood("testFood","testSource","testPrice");
-        objOrderTABLE.addOrder("testOfficer","testDesk","testFood","testItem");
-
-    } //test Add value
 
 }   //Main Class
 
